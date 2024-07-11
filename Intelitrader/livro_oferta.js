1740156dc169398a5a9361005b1a3afc91582491
Dallacqua90125 <156970429+Dallacqua90125@users.noreply.github.com>
2024-07-10 23:53:32 -0300

    Update livro_oferta.js

--- Intelitrader/livro_oferta.js
+++ Intelitrader/livro_oferta.js
@@ -1,96 +1,119 @@
-class Livros {
-    constructor(array) {
-        this.array = array;
+class Order{
+    constructor(position, action, value, quantity){
+        this.position = position;
+        this.action = action;
+        this.value = value;
+        this.quantity = quantity;
     }
+    toString(){
+        return `${this.position}, ${this.value}, ${this.quantity}`;
+    }
+}
+class OrderBook{
+    constructor(){
+       this.orders = [];
 
-    main() {
-        const result = [];
-        for (let i = 0; i < this.array.length; i += 4) {
-            const group = {
-                posicao: this.array[i],
-                acao: this.array[i + 1],
-                valores: this.array[i + 2],
-                quantidade: this.array[i + 3]
-            };
-            if (group.acao === 0) {
-                this.acao0(result, group);
-            } else if (group.acao === 1) {
-                this.acao1(result, group);
-            } else if (group.acao === 2) {
-                this.acao2(result, group);
-            }
+    }
+    updateOrderBook(position, action, value, quantity){
+        const newOrder = new Order(position, action, value, quantity);
+        switch (action) {
+            case 0:
+                this.insertOrder(newOrder);
+                break;
+            case 1:
+                this.modifyOrder(newOrder);
+                break;
+            case 2 :
+                this.deleteOrder(newOrder);
+                break;
         }
-        return result;
     }
-
-    acao0(result, group) {
-        if (!this.verificarPosicao(result, group)) {
-            result.push(group);
+    insertOrder(order){
+        if (!this.verifiPosition(order)) {
+            this.orders.push(order)
         }
     }
-
-    acao1(result, group) {
-        for (let i = 0; i < result.length; i++) {
-            if (result[i].posicao === group.posicao) {
-                if (group.quantidade === 0) {
-                    group.quantidade = result[i].quantidade;
-                } else {
-                    result[i].quantidade = group.quantidade;
+    modifyOrder(order){
+        for (let i = 0; i < this.orders.length; i++) {
+            if (this.orders[i].position === order.position) {
+                if (order.value === 0) {
+                    order.value = this.orders[i].value
+                }else{
+                    this.orders[i].value = order.value
                 }
-                if (group.valores === 0) {
-                    group.valores = result[i].valores;
-                } else {
-                    result[i].valores = group.valores;
+                if (order.quantity === 0) {
+                    order.quantity = this.orders[i].quantity
+                }else{
+                    this.orders[i].quantity = order.quantity
                 }
+                return
+            }   
+        }
+    }
+    deleteOrder(order){
+        for (let i = 0; i < this.orders.length; i++) {
+            if (this.orders[i].position === order.position) {
+                this.orders.splice(i, 1);
+                this.realingPosition();
                 return;
             }
+            
         }
     }
-
-    acao2(result, group) {
-        for (let i = 0; i < result.length; i++) {
-            if (result[i].posicao === group.posicao) {
-                result.splice(i, 1);
-                this.realinharPosicoes(result);
-                return;
+    realingPosition(){
+        for (let i = 0; i < this.orders.length; i++) {
+            this.orders[i].position = i + 1
+            
+        }
+    }
+    verifiPosition(order){
+        for (let i = 0; i < this.orders.length; i++) {
+            if (this.orders[i].position === order.position) {
+                this.orders[i] = order
+                return true
             }
+            
         }
+        return false
     }
 
-    verificarPosicao(result, group) {
-        for (let i = 0; i < result.length; i++) {
-            if (result[i].posicao === group.posicao) {
-                result[i] = group;
-                return true;
-            }
-        }
-        return false;
-    }
-
-    realinharPosicoes(result) {
-        for (let i = 0; i < result.length; i++) {
-            result[i].posicao = i + 1;
-        }
+    showOrderBook() {
+        this.orders.forEach(order => {
+            console.log(order.toString());
+        });
     }
 }
 
-const array = [
-    1, 0, 15.4, 50,
-    2, 0, 15.5, 50,
-    2, 2, 0, 0,
-    2, 0, 15.4, 10,
-    3, 0, 15.9, 30,
-    3, 1, 0, 20,
-    4, 0, 16.50, 200,
-    5, 0, 17.00, 100,
-    5, 0, 16.59, 20,
-    6, 2, 0, 0,
-    1, 2, 0, 0,
-    2, 1, 15.6, 0
-];
+class Program {
+    static main() {
+        const array = [
+            1, 0, 15.4, 50,
+            2, 0, 15.5, 50,
+            2, 2, 0, 0,
+            2, 0, 15.4, 10,
+            3, 0, 15.9, 30,
+            3, 1, 0, 20,
+            4, 0, 16.50, 200,
+            5, 0, 17.00, 100,
+            5, 0, 16.59, 20,
+            6, 2, 0, 0,
+            1, 2, 0, 0,
+            2, 1, 15.6, 0
+        ];
 
-const livros = new Livros(array);
-const livrosP = livros.main();
-livrosP.forEach(item => {
-    console.log(`${item.posicao},${item.valores},${item.quantidade}`);
-});
+        const orderBook = new OrderBook();
+
+        for (let i = 0; i < array.length; i += 4) {
+            const position = array[i];
+            const action = array[i + 1];
+            const value = array[i + 2];
+            const quantity = array[i + 3];
+
+            orderBook.updateOrderBook(position, action, value, quantity);
+        }
+
+        orderBook.showOrderBook();
+    }
+}
+
+Program.main();
